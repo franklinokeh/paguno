25d1f12c414bb12fe111442807586f3ecb29f53f
Franklin okeh <frankok101@gmail.com>
2019-03-11 12:38:00 +0100

    First Draft

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/PagunoGTB'));
+app.use(express.static('./dist/paguno'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/PagunoGTB/index.html'));
+res.sendFile(path.join(__dirname,'/dist/paguno/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
